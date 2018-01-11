ff48cc9ad6fc96d0a4d5a1036a9b329b5a767df4
Daniel Kowalski <danielk.biznes@gmail.com>
2018-01-11 16:50:32 +0100

    First init.

---
+++ core/src/me/daniel/MyGame.java
@@ -0,0 +1,15 @@
+package me.daniel;
+
+import com.badlogic.gdx.Game;
+
+public class MyGame extends Game {
+
+	public static int WIDTH = 1280, HEIGHT = 720;
+	public static String TITLE = "FAT PUG THE GAME";
+
+	@Override
+	public void create() {
+
+	}
+
+}
---
+++ desktop/src/me/daniel/desktop/DesktopLauncher.java
@@ -0,0 +1,20 @@
+package me.daniel.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.daniel.MyGame;
+
+public class DesktopLauncher {
+
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.width = MyGame.WIDTH;
+		config.height = MyGame.HEIGHT;
+		config.resizable = false;
+		config.title = MyGame.TITLE;
+
+		new LwjglApplication(new MyGame(), config);
+	}
+
+}
