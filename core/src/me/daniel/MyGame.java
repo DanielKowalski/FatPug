af113f1eeaf919bdb0c9d36dd557bcba7a887cee
Daniel Kowalski <danielk.biznes@gmail.com>
2018-01-11 20:04:07 +0100

    Created abstract screen class.

--- core/src/me/daniel/MyGame.java
+++ core/src/me/daniel/MyGame.java
@@ -6,10 +6,22 @@ public class MyGame extends Game {
 
 	public static int WIDTH = 1280, HEIGHT = 720;
 	public static String TITLE = "FAT PUG THE GAME";
+	private boolean paused;
 
 	@Override
 	public void create() {
 
 	}
 
+	/*
+		Getters and setters
+	 */
+
+	public boolean isPaused() {
+		return paused;
+	}
+
+	public void setPaused(boolean paused) {
+		this.paused = paused;
+	}
 }
---
+++ core/src/me/daniel/screens/AbstractScreen.java
@@ -0,0 +1,80 @@
+package me.daniel.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+
+import me.daniel.MyGame;
+
+/**
+ * Created by daniel on 11.01.2018.
+ */
+
+public abstract class AbstractScreen implements Screen {
+
+    protected MyGame game;
+    protected Stage stage;
+    private OrthographicCamera camera;
+
+    public AbstractScreen(MyGame game) {
+        createCamera();
+        this.game = game;
+        stage = new Stage(new StretchViewport(MyGame.WIDTH, MyGame.HEIGHT));
+        init();
+    }
+
+    protected abstract void init();
+
+    private void createCamera() {
+        camera = new OrthographicCamera(MyGame.WIDTH, MyGame.HEIGHT);
+        camera.setToOrtho(true);
+        camera.update();
+    }
+
+    @Override
+    public void render(float delta) {
+        clearScreen();
+        camera.update();
+
+        if(!game.isPaused())stage.act();
+        stage.draw();
+    }
+
+    private void clearScreen() {
+        Gdx.gl20.glClearColor(0, 0, 0, 0);
+        Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void pause() {
+        game.setPaused(true);
+    }
+
+    @Override
+    public void resume() {
+        game.setPaused(false);
+    }
+
+    @Override
+    public void dispose() {
+        game.dispose();
+        stage.dispose();
+    }
+
+    /*
+        Unused unimplemented methods
+     */
+
+    @Override
+    public void show() {}
+
+    @Override
+    public void hide() {}
+
+    @Override
+    public void resize(int width, int height) {}
+
+}
